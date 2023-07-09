e1387ac6a9b89699e10aeed6a6ad6cf1d36ad31b
Sumedh <sumedhvyas10@gmail.com>
2023-07-09 21:41:34 +0530

    first

---
+++ hjw1.js
@@ -0,0 +1,20 @@
+// var and function declaration 
+
+
+// console.log(this)
+// console.log(window)
+// console.log(firstName)
+// var firstName="Sumedh"
+// console.log(firstName)
+
+console.log(this)
+console.log(this)
+console.log(myfunction)
+console.log(fullname)
+function myfunction(){
+    console.log("this is a function")
+}
+var firstName="S"
+var lastName="V"
+var fullname=firstName + " " +lastName
+console.log(fullname)
---
+++ hjw2.js
@@ -0,0 +1,13 @@
+// var and function expression 
+
+console.log(this)
+console.log(this)
+console.log(myfunction)
+console.log(fullname)
+function myfunction(){
+    console.log("this is a function")
+}
+var firstName="S"
+var lastName="V"
+var fullname=firstName + " " +lastName
+console.log(fullname)
